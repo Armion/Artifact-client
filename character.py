11c45b1247dbaf56cf70cced6d534d1f96d1ad84
Mathieu <zarmion@gmail.com>
2024-08-02 11:33:49 +0200

    chore: Optimized code

--- character.py
+++ character.py
@@ -1,7 +1,8 @@
 from connexion import Connexion
 from map import Map
 import dpath.util
-import time
+from time import sleep
+from tqdm import tqdm
 
 class Character:
     def __init__(self, character_name):
@@ -22,37 +23,46 @@ class Character:
     def get_data(self):
         self.data = dpath.util.get(self.connexion.get(f"characters/{self.character_name}"), 'data')
 
-    def update_data(self):
-        self.get_data()
+    def update_data(self, fetch = True):
+        if fetch == True:
+            self.get_data()
+        
         self.hp = self.data['hp']
         self.lvl = self.data['level']
         self.gold = self.data['gold']
-        self.current_xp = self.data['total_xp']
+        self.current_xp = self.data['xp']
         self.next_lvl_xp = self.data['max_xp']
+        self.needed_xp = self.next_lvl_xp - self.current_xp
         self.cooldown = self.data['cooldown']
         self.pos_x = self.data['x']
         self.pos_y = self.data['y']
 
     def fight(self, verbose = True):
         self.wait_for_cd()
-        self.connexion.post(f"my/{self.character_name}/action/fight")
+        print("starting the fight !")
+        self.data = dpath.util.get(self.connexion.post(f"my/{self.character_name}/action/fight"), 'data/character')
+        self.update_data(False)
+        
         if verbose == True:
-            self.update_data()
             self.display_character()
 
     def display_character(self):
         print(f" lvl : {self.lvl}")
         print(f"hp : {self.hp}")
-        print(f"xp to next lvl: {self.next_lvl_xp}")
+        print(f"xp to next lvl: {self.needed_xp}")
         print(f"coordinates x: {self.pos_x} y: {self.pos_y}")
         print(f"gold : {self.gold}")
 
     def wait_for_cd(self):
-        time.sleep(float(self.cooldown) + 0.5)
+        total_time = float(self.cooldown) + 0.5
+        print(f"Waiting CD of {total_time} seconds")
+
+        for _ in tqdm(range(int(total_time * 10)), desc="Cooldown Progress", unit="0.1s"):
+            sleep(0.1)
+
         self.get_data()
 
     def farm_chicken(self):
-        self.wait_for_cd()
         print("Moving to the nearest chickens spot !")
         self.travel_to_nearest_monster('chicken')
 
@@ -66,6 +76,7 @@ class Character:
 
         if self.pos_x == nearest['x'] and self.pos_y == nearest['y']:
             print("You are already there !")
+            self.update_data()
             return None
         else:
             self.move(nearest['x'], nearest['y'])
