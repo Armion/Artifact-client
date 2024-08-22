92a460a1adc956061659ab59167fad2aaaf6ff22
Mathieu <zarmion@gmail.com>
2024-08-22 14:42:20 +0200

    Changed handling of cooldown

--- character/character.py
+++ character/character.py
@@ -1,29 +1,24 @@
 from connexion import Connexion
 from errors.exceptions import CharacterNotFoundError
 from tools.colors import print_color, Color
+from tools.server import Server
 from map import Map
 from items.inventory import Inventory
 
-import dpath.util
+import dpath
 from time import sleep
 from tqdm import tqdm
 import requests
+from datetime import datetime, timedelta
 
 class Character:
-    BLUE = '\033[94m'
-    RED = '\033[91m'
-    YELLOW = '\033[93m'
-    GREEN = '\033[92m'
-    WHITE = '\033[97m'
-    BLACK = '\033[97m'
-    RESET = '\033[0m'
-
     def __init__(self, character_name):
         self.character_name = character_name
         self.connexion = Connexion()
+        self.server = Server()
         self.update_data()
 
-    def move(self, x, y):
+    def move(self, x: int, y: int) -> None:
         self.connexion.post(
             f"my/{self.character_name}/action/move",
                 {
@@ -33,10 +28,10 @@ class Character:
             )
         self.update_data()
 
-    def get_data(self):
+    def get_data(self) -> None:
         try:
             response = self.connexion.get(f"characters/{self.character_name}")
-            self.data = dpath.util.get(response, 'data')
+            self.data = dpath.get(response, 'data')
             self.inventory = Inventory(self.data)
         except requests.exceptions.HTTPError as e:
             if e.response.status_code == 404:
@@ -44,7 +39,7 @@ class Character:
             else:
                 raise e
 
-    def update_data(self, fetch = True):
+    def update_data(self, fetch : bool = True) -> None:
         if fetch == True:
             self.get_data()
         
@@ -55,54 +50,63 @@ class Character:
         self.next_lvl_xp = self.data['max_xp']
         self.needed_xp = self.next_lvl_xp - self.current_xp
         self.cooldown = self.data['cooldown']
+        self.cooldown_expiration = datetime.strptime(
+            self.data['cooldown_expiration'],
+            "%Y-%m-%dT%H:%M:%S.%fZ"
+        )
         self.pos_x = self.data['x']
         self.pos_y = self.data['y']
 
-    def fight(self, verbose = True):
+    def fight(self, verbose: bool = True) -> None:
         self.wait_for_cd()
         print("starting the fight !")
-        self.data = dpath.util.get(self.connexion.post(f"my/{self.character_name}/action/fight"), 'data/character')
+        self.data = dpath.get(self.connexion.post(f"my/{self.character_name}/action/fight"), 'data/character')
         self.update_data(False)
         
         if verbose == True:
             self.display_character()
 
-    def display_character(self, verbose = False):
+    def display_character(self, verbose: bool = False) -> None:
         print_color(f"lvl : {self.lvl}", Color.BLUE)
         print_color(f"hp : {self.hp}", Color.RED)
         print_color(f"xp to next lvl: {self.needed_xp}", Color.BLUE)
-        print(f"coordinates x: {self.pos_x} y: {self.pos_y}")
         print_color(f"gold : {self.gold}", Color.YELLOW)
 
         if(verbose):
             self.inventory.display()
+            print(f"coordinates x: {self.pos_x} y: {self.pos_y}")
 
-    def wait_for_cd(self):
-        total_time = float(self.cooldown) + 0.5
-        print(f"Waiting CD of {total_time} seconds")
+    def wait_for_cd(self) -> None:
+        remaining_seconds = (self.cooldown_expiration - self.server.get_server_current_time()).total_seconds() + 0.5
 
-        for _ in tqdm(range(int(total_time * 10)), desc="Cooldown Progress", unit="0.1s"):
+        if (remaining_seconds <= 0):
+            print("No CD to wait for !")
+            return None
+        
+        print(f"Waiting CD of {remaining_seconds} seconds")
+
+        for _ in tqdm(range(int(remaining_seconds * 10)), desc="Cooldown Progress", unit="0.1s"):
             sleep(0.1)
 
         self.get_data()
 
-    def farm_monster(self, name='chicken'):
+    def farm_monster(self, name: str ='chicken') -> None:
         print(f"Moving to the nearest {name} spot !")
         self.travel_to_nearest_object(name)
 
         while(True):
             self.fight()
 
-    def travel_to_nearest_object(self, name, online = False):
+    def travel_to_nearest_object(self, name: str, online: bool = False) -> None:
         map = Map('online') if online else Map('offline')
         nearest = map.find_nearest_object(self.pos_x, self.pos_y, name)
-        self.wait_for_cd()
 
         if self.pos_x == nearest['x'] and self.pos_y == nearest['y']:
             print("You are already there !")
             self.update_data()
             return None
         else:
+            self.wait_for_cd()
             self.move(nearest['x'], nearest['y'])
         return None
 
--- tools/server.py
+++ tools/server.py
@@ -5,22 +5,41 @@ import dpath.util
 class Server:
     def __init__(self) -> None:
         self.connexion = Connexion()
+        self.delta = None
         self.__fetch_data__()
 
     def __fetch_data__(self) -> None:
         self.data = self.connexion.get('')
+        self.__parse_date__()
+        self.delta_time(sync=False)
 
-    def get_server_time(self) -> datetime:
-        self.__fetch_data__()
-        self.server_time = datetime.strptime(
-            dpath.get(self.data, 'data/server_time'),
-            "%Y-%m-%dT%H:%M:%S.%fZ"
-        )
+    def get_server_time(self, sync: bool = True) -> datetime:
+        if(sync):
+            self.__fetch_data__()
+        
+        self.__parse_date__()
         return self.server_time
 
+    def get_server_current_time(self, sync: bool = False) -> datetime:
+        if (not sync):
+            self.current_time = datetime.now() + self.delta_time(sync=False)
+        else:
+            self.current_time = datetime.now() + self.delta_time()
+        return self.current_time
+
     def delta_time(self, sync = True) -> timedelta:
-        if (not sync and self.delta is not None):
+        if (not sync):
+            if self.delta is None:
+                self.delta = self.server_time - datetime.now()
             return self.delta
         
         self.delta = self.get_server_time() - datetime.now()
         return self.delta
+    
+    def __parse_date__(self) -> None:
+        self.server_time = datetime.strptime(
+            dpath.get(self.data, 'data/server_time'),
+            "%Y-%m-%dT%H:%M:%S.%fZ"
+        )
+
+        return None
